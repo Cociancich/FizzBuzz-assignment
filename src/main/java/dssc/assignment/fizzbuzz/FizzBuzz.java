f56095a789055c3e86bbccad26df701130561cef
Cociancich <alexia.cociancich@gmail.com>
2024-11-02 19:05:48 +0100

    print implemented

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,9 @@
 package dssc.assignment.fizzbuzz;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class FizzBuzz {
 
     public static void main(String... args) {
@@ -7,7 +11,10 @@ public class FizzBuzz {
     }
 
     public String convert(int number) {
-        if (number % 3 == 0){
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0){
             return "Fizz";
         }
         else if (number % 5 == 0){
@@ -16,5 +23,10 @@ public class FizzBuzz {
         else {
             return Integer.toString(number);
         }
+    }
+
+    public void print() {
+        Stream<String> fizzBuzzes =  IntStream.range(1, 16).mapToObj(this::convert);
+        System.out.print(fizzBuzzes.collect(Collectors.joining(", ")));
     }
 }
--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -18,15 +18,22 @@ public class Convert {
 
     @ParameterizedTest
     @ValueSource(ints =  {3, 6, 24} )
-    void multiples_of_three_to_Fizz(int number) {
+    void multiples_of_3_to_Fizz(int number) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.convert(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints =  {5, 10, 100} )
-    void multiples_of_five_to_Fizz(int number) {
+    void multiples_of_5_to_Buzz(int number) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Buzz", fizzBuzz.convert(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints =  {15, 45, 75} )
+    void multiples_of_3_and_5_to_FizzBuzz(int number) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("FizzBuzz", fizzBuzz.convert(number));
     }
 }
